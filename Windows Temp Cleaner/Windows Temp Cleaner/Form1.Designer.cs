ef40ab8be4e21dc3f9b309c49ca74709dbfae962
Neaz Imam <110326562+Neazimam@users.noreply.github.com>
2025-08-10 08:56:27 -0700

    Add files via upload

---
+++ Windows Temp Cleaner/Windows Temp Cleaner/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Windows_Temp_Cleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.windows = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.guna2ProgressBar1 = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.windows.SuspendLayout();
+            this.guna2GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(9, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Prefetch";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox2.Location = new System.Drawing.Point(9, 53);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(55, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Temp";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox3.Location = new System.Drawing.Point(9, 78);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(74, 19);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "%temp%";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox4.Location = new System.Drawing.Point(9, 103);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(76, 19);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Junk Files";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Red;
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(376, 12);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(39, 28);
+            this.guna2ControlBox1.TabIndex = 5;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.Animated = true;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.Lime;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
+            this.guna2Button1.IndicateFocus = true;
+            this.guna2Button1.Location = new System.Drawing.Point(149, 351);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(114, 46);
+            this.guna2Button1.TabIndex = 6;
+            this.guna2Button1.Text = "CleanUp";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.8D;
+            this.guna2DragControl1.TargetControl = this;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox5.Location = new System.Drawing.Point(17, 28);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(120, 19);
+            this.checkBox5.TabIndex = 7;
+            this.checkBox5.Text = "Cinema 4D Cache";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox6.Location = new System.Drawing.Point(17, 53);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(105, 19);
+            this.checkBox6.TabIndex = 8;
+            this.checkBox6.Text = "Redshift Cache";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox7.Location = new System.Drawing.Point(17, 78);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(91, 19);
+            this.checkBox7.TabIndex = 9;
+            this.checkBox7.Text = "Maya Cache";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Temporary File Cleaner For Artist";
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.FillColor = System.Drawing.Color.Lime;
+            this.guna2Separator1.Location = new System.Drawing.Point(3, 39);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(423, 18);
+            this.guna2Separator1.TabIndex = 11;
+            // 
+            // windows
+            // 
+            this.windows.BorderColor = System.Drawing.Color.White;
+            this.windows.BorderRadius = 1;
+            this.windows.Controls.Add(this.checkBox1);
+            this.windows.Controls.Add(this.checkBox2);
+            this.windows.Controls.Add(this.checkBox3);
+            this.windows.Controls.Add(this.checkBox4);
+            this.windows.CustomBorderColor = System.Drawing.Color.Chartreuse;
+            this.windows.CustomBorderThickness = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.windows.FillColor = System.Drawing.Color.Black;
+            this.windows.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.windows.ForeColor = System.Drawing.Color.Black;
+            this.windows.Location = new System.Drawing.Point(15, 63);
+            this.windows.Name = "windows";
+            this.windows.Size = new System.Drawing.Size(183, 128);
+            this.windows.TabIndex = 13;
+            this.windows.Text = "Windows";
+            this.windows.TextOffset = new System.Drawing.Point(0, -10);
+            // 
+            // guna2GroupBox1
+            // 
+            this.guna2GroupBox1.BorderColor = System.Drawing.Color.White;
+            this.guna2GroupBox1.Controls.Add(this.checkBox5);
+            this.guna2GroupBox1.Controls.Add(this.checkBox6);
+            this.guna2GroupBox1.Controls.Add(this.checkBox7);
+            this.guna2GroupBox1.CustomBorderColor = System.Drawing.Color.DodgerBlue;
+            this.guna2GroupBox1.CustomBorderThickness = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.guna2GroupBox1.FillColor = System.Drawing.Color.Black;
+            this.guna2GroupBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2GroupBox1.ForeColor = System.Drawing.Color.Black;
+            this.guna2GroupBox1.Location = new System.Drawing.Point(224, 63);
+            this.guna2GroupBox1.Name = "guna2GroupBox1";
+            this.guna2GroupBox1.Size = new System.Drawing.Size(183, 128);
+            this.guna2GroupBox1.TabIndex = 14;
+            this.guna2GroupBox1.Text = "Cache From Tools";
+            this.guna2GroupBox1.TextOffset = new System.Drawing.Point(0, -10);
+            // 
+            // guna2ProgressBar1
+            // 
+            this.guna2ProgressBar1.Location = new System.Drawing.Point(61, 332);
+            this.guna2ProgressBar1.Name = "guna2ProgressBar1";
+            this.guna2ProgressBar1.Size = new System.Drawing.Size(300, 13);
+            this.guna2ProgressBar1.TabIndex = 15;
+            this.guna2ProgressBar1.Text = "guna2ProgressBar1";
+            this.guna2ProgressBar1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(358, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 12);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Dev-!EAGLE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(427, 445);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guna2ProgressBar1);
+            this.Controls.Add(this.guna2GroupBox1);
+            this.Controls.Add(this.windows);
+            this.Controls.Add(this.guna2Separator1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.windows.ResumeLayout(false);
+            this.windows.PerformLayout();
+            this.guna2GroupBox1.ResumeLayout(false);
+            this.guna2GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2GroupBox windows;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2ProgressBar guna2ProgressBar1;
+    }
+}
+
---
+++ Windows Temp Cleaner/Windows Temp Cleaner/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO; // Needed for file and folder operations
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Temp_Cleaner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // No action needed here unless you want live behavior.
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            // No action needed here unless you want live behavior.
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            // No action needed here unless you want live behavior.
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            // You can define this one too if you want.
+        }
+
+       
+
+        private void DeleteFilesInFolder(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                foreach (string file in Directory.GetFiles(folderPath))
+                {
+                    try
+                    {
+                        File.Delete(file);
+                    }
+                    catch (Exception) { /* You can log errors if you want */ }
+                }
+
+                foreach (string dir in Directory.GetDirectories(folderPath))
+                {
+                    try
+                    {
+                        Directory.Delete(dir, true);
+                    }
+                    catch (Exception) { /* You can log errors if you want */ }
+                }
+            }
+        }
+        private void CleanCinema4DCache()
+        {
+            try
+            {
+                string roamingAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                // 1. Clean C4D Temp folder
+                string c4dTempPath = Path.Combine(localAppData, "Temp", "MAXON");
+                if (Directory.Exists(c4dTempPath))
+                {
+                    DeleteFilesInFolder(c4dTempPath);
+                }
+
+                // 2. Search for Cinema 4D versions in Roaming\MAXON\
+                string maxonRoamingPath = Path.Combine(roamingAppData, "MAXON");
+
+                if (Directory.Exists(maxonRoamingPath))
+                {
+                    var cinema4DFolders = Directory.GetDirectories(maxonRoamingPath)
+                                                   .Where(dir => dir.Contains("2023") || dir.Contains("2024") || dir.Contains("R24"))
+                                                   .ToList();
+
+                    foreach (string c4dVersionFolder in cinema4DFolders)
+                    {
+                        // Clean _bugreports
+                        string bugReportsPath = Path.Combine(c4dVersionFolder, "_bugreports");
+                        if (Directory.Exists(bugReportsPath))
+                        {
+                            DeleteFilesInFolder(bugReportsPath);
+                        }
+
+                        // Clean _prefs\previewimages
+                        string previewImagesPath = Path.Combine(c4dVersionFolder, "_prefs", "previewimages");
+                        if (Directory.Exists(previewImagesPath))
+                        {
+                            DeleteFilesInFolder(previewImagesPath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error cleaning Cinema 4D cache: " + ex.Message);
+            }
+        }
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkBox1.Checked) // Prefetch
+                {
+                    DeleteFilesInFolder(@"C:\Windows\Prefetch");
+                }
+
+                if (checkBox2.Checked) // Temp
+                {
+                    DeleteFilesInFolder(@"C:\Windows\Temp");
+                }
+
+                if (checkBox3.Checked) // User Local Temp
+                {
+                    string userTempPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Temp");
+                    DeleteFilesInFolder(userTempPath);
+                }
+
+                if (checkBox4.Checked) // Junk Files
+                {
+                    DeleteFilesInFolder(@"C:\Windows\SoftwareDistribution\Download");
+
+                    string inetCachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Microsoft\Windows\INetCache");
+                    DeleteFilesInFolder(inetCachePath);
+
+                    string windowsCachesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Microsoft\Windows\Caches");
+                    DeleteFilesInFolder(windowsCachesPath);
+
+                    string crashDumpsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CrashDumps");
+                    DeleteFilesInFolder(crashDumpsPath);
+
+                    DeleteFilesInFolder(@"C:\$Recycle.Bin"); // Optional
+                }
+                if (checkBox5.Checked) // Cinema 4D cache
+                {
+                    CleanCinema4DCache();
+                }
+                if (checkBox6.Checked) // Redshift Cache
+                {
+                    string redshiftFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Redshift");
+                    DeleteFilesInFolder(redshiftFolderPath);
+                }
+                if (checkBox7.Checked)
+                {
+                    string redshiftFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Redshift");
+                    DeleteFilesInFolder(redshiftFolderPath);
+                }
+                MessageBox.Show("Selected directories cleaned successfully!", "Cleanup Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during cleanup: " + ex.Message);
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
